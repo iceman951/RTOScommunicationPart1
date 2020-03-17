fb5dd4613d9548b9822fefcf29b335b2a1a4f95f
iceman951 <47938031+iceman951@users.noreply.github.com>
2020-03-17 10:35:03 +0700

    First Commit
    UART 115200 ส่งข้อมูล Digital pin 0-13 ทุกๆ 300 ms และ Analog Pin 0-5 ทุกๆ 900 ms FreeRTOS

---
+++ ch1.ino
@@ -0,0 +1,39 @@
+#include <Arduino_FreeRTOS.h> 
+
+void setup() {
+  Serial.begin(115200);
+  xTaskCreate(TaskWriteAnalog, "AnalogWrite", 128, NULL, 2, NULL);
+  xTaskCreate(TaskWriteDigital, "DigitalWrite", 128, NULL, 1, NULL);
+  vTaskStartScheduler();
+}
+
+void TaskWriteAnalog( void *pvParameters ) //priority = 1
+{ (void) pvParameters;
+ int i;
+  for (;;){
+  for(i=0;i<6;i++)
+  {
+    Serial.print("Analog ");
+    Serial.print(i);
+    Serial.print(" : ");
+    Serial.println(analogRead(i));
+  }
+  vTaskDelay(pdMS_TO_TICKS(900));
+ }
+}
+
+void TaskWriteDigital( void *pvParameters ) //priority = 1
+{ (void) pvParameters;
+ int i;
+  for (;;){
+  for(i=1;i<14;i++)
+  {
+    Serial.print("Digitial ");
+    Serial.print(i);
+    Serial.print(" : ");
+    Serial.println(digitalRead(i));
+  }vTaskDelay(pdMS_TO_TICKS(300));
+ }
+}
+
+void loop(){}
